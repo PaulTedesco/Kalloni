01d4c6731795ac3d423ef5f8cdb0fadfdf25d4bd
steodec <paul.tedesco@ynov.com>
2021-02-15 19:08:30 +0100

    Update DB

--- src/database/ynov_ydays.sql
+++ src/database/ynov_ydays.sql
@@ -1,52 +1,39 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : lun. 15 fév. 2021 à 07:55
--- Version du serveur :  8.0.21
--- Version de PHP : 8.0.1
+create table items
+(
+    id          char(36)     not null
+        primary key,
+    title       varchar(255) not null,
+    description longtext     not null,
+    picture     varchar(255) not null,
+    category    char(36)     not null,
+    created_at  varchar(255) not null,
+    updated_at  varchar(255) not null,
+    price       varchar(255) not null
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table items_cat
+(
+    id         char(36)     not null
+        primary key,
+    title      varchar(255) not null,
+    created_at varchar(255) not null,
+    updated_at varchar(255) not null
+);
 
+create table users
+(
+    id         char(36)                      not null
+        primary key,
+    first_name varchar(255)                  not null,
+    last_name  varchar(255)                  not null,
+    email      varchar(255)                  not null,
+    password   varchar(255)                  not null,
+    zip_code   varchar(255)                  not null,
+    city       varchar(255)                  not null,
+    country    varchar(255)                  not null,
+    address    varchar(255)                  not null,
+    roles      varchar(255) default 'membre' not null,
+    created_at varchar(255)                  not null,
+    updated_at varchar(255)                  not null
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `ynov_ydays`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` char(36) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `zip_code` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `country` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `roles` varchar(255) NOT NULL DEFAULT 'membre',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Déchargement des données de la table `users`
---
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
